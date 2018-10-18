065848b7d7ecc72f3959a9753cf838418c83b658
yulissateran <yulissa.lteran@gmail.com>
2018-10-18 00:15:59 -0500

    add service worker OK.

--- src/app/sw.js
+++ src/app/sw.js
@@ -1,33 +1,33 @@
-importScripts('/cache-polyfill.js');
+// importScripts('/cache-polyfill.js');
 
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
-       '/styles/main.css',
-       '/scripts/main.min.js',
-       '/sounds/airhorn.mp3'
-     ]);
-   })
- );
-});
-self.addEventListener('fetch', function(event) {
+// self.addEventListener('install', function(e) {
+//  e.waitUntil(
+//    caches.open('airhorner').then(function(cache) {
+//      return cache.addAll([
+//        '/',
+//        '/index.html',
+//        '/index.html?homescreen=1',
+//        '/?homescreen=1',
+//        '/styles/main.css',
+//        '/scripts/main.min.js',
+//        '/sounds/airhorn.mp3'
+//      ]);
+//    })
+//  );
+// });
+// self.addEventListener('fetch', function(event) {
 
-  console.log(event.request.url);
+//   console.log(event.request.url);
   
-  event.respondWith(
+//   event.respondWith(
   
-  caches.match(event.request).then(function(response) {
+//   caches.match(event.request).then(function(response) {
   
-  return response || fetch(event.request);
+//   return response || fetch(event.request);
   
-  })
+//   })
   
-  );
+//   );
   
-  });
+//   });
