2faef1021a179a2dc08bfc2d455422eed6f11a7a
yulissateran <yulissa.lteran@gmail.com>
2018-10-19 17:57:16 -0500

    add service worker .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 // import { Component } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { SwUpdate } from '@angular/service-worker';
 
 export interface Item { name: string; }
 
-// @Component({
-//   selector: 'app-root',
-//   template: `
-//     <div>
-//       {{ (item | async)?.name }}
-//     </div>
-//   `
-// })
-// export class AppComponent {
-  
-// }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'burger-queen';
 
-  constructor() {
+  constructor(private swUpdate: SwUpdate) {
   }
+
+ ngOnInit() {
+  if (this.swUpdate.isEnabled) {
+    this.swUpdate.available.subscribe(() => {
+      window.location.reload();
+    });
+  }
+ }
+  
 
 }
